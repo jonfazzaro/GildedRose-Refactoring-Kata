c4838c2987c01e1f08bb0871e8c64263736ea25c
Jon Fazzaro <JFazzaro@apterainc.com>
2016-01-05 09:32:46 -0500

    Green tests, ugly code. Go.

---
+++ csharp/GildedRose/GildedRose/ApprovalTest.cs
@@ -0,0 +1,25 @@
+﻿namespace GildedRoseTests {
+    using ApprovalTests;
+    using ApprovalTests.Reporters;
+    using GildedRose;
+    using NUnit.Framework;
+    using System;
+    using System.IO;
+    using System.Text;
+
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class ApprovalTest {
+        [Test]
+        public void ThirtyDays() {
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            String output = fakeoutput.ToString();
+            Approvals.Verify(output);
+        }
+    }
+
+}
