ce782ab383ac6d023f42f2979a875b3874b8044e
Jon Fazzaro <JFazzaro@apterainc.com>
2016-01-05 09:49:07 -0500

    Move Item class to its own file.
    Per requirements, no actual changes to the Item class itself. Happy Corner Goblin.

--- csharp/GildedRose/GildedRose/GildedRose.cs
+++ csharp/GildedRose/GildedRose/GildedRose.cs
@@ -61,12 +61,4 @@ namespace GildedRose {
 
     }
 
-    public class Item {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
-    }
-
 }
