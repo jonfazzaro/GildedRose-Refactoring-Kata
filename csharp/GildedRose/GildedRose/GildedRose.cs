663d6c2580af5d9b246c2a27e5ba5f13fa3d242e
Jon Fazzaro <JFazzaro@apterainc.com>
2016-01-05 09:55:07 -0500

    Factor out UpdatableItem and move updating logic out of the GildedRose class.

--- csharp/GildedRose/GildedRose/GildedRose.cs
+++ csharp/GildedRose/GildedRose/GildedRose.cs
@@ -1,67 +1,15 @@
-using System.Collections.Generic;
-
 namespace GildedRose {
+    using System.Collections.Generic;
+
     class GildedRose {
-        IList<Item> Items;
-        public GildedRose(IList<Item> Items) {
-            this.Items = Items;
+        IList<UpdatableItem> Items;
+        public GildedRose(IList<UpdatableItem> items) {
+            this.Items = items;
         }
 
         public void UpdateQuality() {
-            foreach (var item in Items) {
-                UpdateQuality(item);
-            }
-        }
-
-        private static void UpdateQuality(Item item) {
-            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.Quality > 0) {
-                    if (item.Name != "Sulfuras, Hand of Ragnaros") {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-            } else {
-                if (item.Quality < 50) {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.SellIn < 11) {
-                            if (item.Quality < 50) {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-
-                        if (item.SellIn < 6) {
-                            if (item.Quality < 50) {
-                                item.Quality = item.Quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.Name != "Sulfuras, Hand of Ragnaros") {
-                item.SellIn = item.SellIn - 1;
-            }
-
-            if (item.SellIn < 0) {
-                if (item.Name != "Aged Brie") {
-                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.Quality > 0) {
-                            if (item.Name != "Sulfuras, Hand of Ragnaros") {
-                                item.Quality = item.Quality - 1;
-                            }
-                        }
-                    } else {
-                        item.Quality = item.Quality - item.Quality;
-                    }
-                } else {
-                    if (item.Quality < 50) {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
+            foreach (var item in Items)
+                item.UpdateQuality();
         }
     }
-
 }
